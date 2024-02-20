88f590e22295ee3555b5eaf2d7c05d12243a6806
aburgos01 <49335817+aburgos01@users.noreply.github.com>
2024-02-19 18:29:51 -0700

    Update main_page.py

--- streamlit_template/main_page.py
+++ streamlit_template/main_page.py
@@ -5,5 +5,5 @@ st.sidebar.markdown("# Main Page")
 
 st.write("Click on a page to see racer or kart stats")
 
-link = '[To my Github Pages](https://aburgos01.github.io/github-assignment/)'
-st.markdown(link, unsafe_allow_html=True)
+st.link = '[To my Github Pages](https://aburgos01.github.io/github-assignment/)'
+st.markdown(st.link, unsafe_allow_html=True)

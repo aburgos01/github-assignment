771a93ff4acfa15d744c4bb8b93f4cde983cdbcc
aburgos01 <49335817+aburgos01@users.noreply.github.com>
2024-02-19 18:01:19 -0700

    Adding Nav Links

--- streamlit_template/main_page.py
+++ streamlit_template/main_page.py
@@ -3,4 +3,7 @@ import streamlit as st
 st.markdown("# Welcome to my Website!")
 st.sidebar.markdown("# Main Page") 
 
-st.write("Click on a page to see racer stats")
+st.write("Click on a page to see racer or kart stats")
+
+link = '[To my Github Pages](https://aburgos01.github.io/github-assignment/)'
+st.markdown(link, unsafe_allow_html=True)
